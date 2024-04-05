bd2b99b6b95ff86308c80d793c248b39509a2ef8
echt16 <albertivanov375@gmail.com>
2024-04-05 17:08:46 +0300

    Last commit

---
+++ practice1part1/Pages/EX5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace practice1part1.Pages
+{
+    public class EX5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
